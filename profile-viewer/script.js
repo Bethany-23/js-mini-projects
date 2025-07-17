abe928c74e2da1bcbc7dfdf3f904f3b67b093e35
Bethany-23 <bethanyaddis7@gmail.com>
2025-07-17 19:25:04 +0300

    Add Profile Viewer project

---
+++ profile-viewer/script.js
@@ -0,0 +1,43 @@
+const display = document.getElementById("profile");
+
+const users = [
+    {
+        name: "David",
+        age: 28,
+        bio: "Trying to make it as a footballer"
+    },
+    {
+        name: "Yasmeen",
+        age: 32,
+        bio: "Astrophysicist, shooting beyond the stars"
+    },
+    {
+        name: "Michelle",
+        age: 26,
+        bio: "Graphics designer"
+    }
+];
+
+let currentIndex = 0;
+
+function showProfile() {
+    const user = users[currentIndex];
+    display.innerHTML = `
+        <h3>Name: ${user.name}</h3>
+        <p>Age: ${user.age}</p>
+        <p>Bio: ${user.bio}</p>
+    `;
+}
+
+function nextProfile() {
+    currentIndex = (currentIndex + 1) % users.length;
+    showProfile();
+}
+
+function prevProfile() {
+    currentIndex = (currentIndex - 1 + users.length) % users.length;
+    showProfile();
+}
+
+// show the first profile when page loads
+showProfile();
