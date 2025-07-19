556c5a1d9a50c965be010fc9754a6816e4077dad
Bethany-23 <bethanyaddis7@gmail.com>
2025-07-19 23:45:39 +0300

    edited css

--- BMI-calculator/script.js
+++ BMI-calculator/script.js
@@ -6,10 +6,16 @@ const display = document.getElementById("display")
 
 function bmi(){
     let result = weight.value/(height.value **2);
+    
     if (result<=19){
         console.log("you're underweight")
 
     }
-
+    else if(result<=25){
+        console.log("you're in the normal range")
+    }
+    else{
+        console.log("you're obese")
+    }
     display.innerHTML = result
 }
