290ca5d3b3cf87564af2dd6b03a12d27b7662e11
Bethany-23 <bethanyaddis7@gmail.com>
2025-08-20 11:41:32 +0300

    style added

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,21 +1,25 @@
 
 const apiKey = "08a6a242a4c519dffbf1034dbcf1a89d";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric"
 
 const temp = document.querySelector(".temp");
 const city = document.querySelector(".city");
 const wind = document.querySelector(".wind")
 const humidity = document.querySelector(".humidity")
 
-async function checkWeather (){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.getElementById("input")
+const searchBtn = document.getElementById("search-btn")
+
+async function checkWeather (city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json;
 
     city.innerHTML = data.name;
-    temp.innerHTML = data.main.temp;
-    wind.innerHTML = data.main.wind;
-    humidity.innerHTML = data.main.humidity;
-
-
-    console.log(data)
+    temp.innerHTML = data.main.temp + "°C";
+    wind.innerHTML = data.main.wind + " km/h";
+    humidity.innerHTML = data.main.humidity + "%";
 }
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+});
