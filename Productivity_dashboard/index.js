2934b2574982552fa91a6f35ec771bdff0f41787
Bethany-23 <bethanyaddis7@gmail.com>
2025-08-23 23:58:13 +0300

    edited pages

--- Productivity_dashboard/index.js
+++ Productivity_dashboard/index.js
@@ -10,15 +10,16 @@ function renderTasks(){
     const filterCategory = document.getElementById("category").value;
     const searchItem = document.getElementById("search").value.toLowerCase();
 
-    tasks.filter(task =>{
+    tasks
+    .filter(task =>{
         const matchesCategory = filterCategory ? task.category === filterCategory: true;
         const matchesSearch = task.text.toLowerCase().includes(searchItem);
         return matchesCategory && matchesSearch;
-    });
-    tasks.sort((a,b)=>{new Date(a.due)- new Date(b.due)});
+    })
+    .sort((a,b)=>{new Date(a.due)- new Date(b.due)});
 
    tasks.forEach((task, index)=>{
-    const list = document.createElement("li")
+    const list = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type= "checkbox"
     checkbox.checked = task.completed;
@@ -62,7 +63,7 @@ function renderTasks(){
     const dueDate = document.getElementById("due-date").value;
     const category = document.getElementById("category").value;
     if(text, dueDate, category){
-        tasks.push({text: text, completed: false, dueDate: dueDate, category: category});
+        tasks.push({text: text, completed: false, due: dueDate, category: category});
         inputTask.value = "";
         dueDate.value = " ";
         saveTasks();
@@ -71,6 +72,9 @@ function renderTasks(){
    });
 
 
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 function updateStats(){
     const total = tasks.length;
@@ -81,11 +85,7 @@ function updateStats(){
 
     const progress = total === 0? 0: (completed/total) * 100;
     document.getElementById("progress-bar").style.width = progress + "%";
-}
-
-function saveTasks(){
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+} 
 function loadTasks(){
     const stored = localStorage.getItem("tasks");
     if(stored){
