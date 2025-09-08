e4199c00d53a05bacb409e0da17a58e628fdbf4d
Bethany-23 <bethanyaddis7@gmail.com>
2025-09-08 22:33:18 +0300

    setup database

---
+++ Movie_site/Backend/index.js
@@ -0,0 +1,23 @@
+
+import app from "./server";
+import mongodb from "mongodb";
+import ReviewsDAO from "./dao/reviewsDAO";
+
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env['MONGO_USERNAME'];
+const mongo_password = process.env['MONGO_PASSWORD'];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.i0ylxiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const port = 8000;
+
+MongoClient.connect(
+    uri, 
+    {
+        maxPoolSize: 50,
+        timeoutMS: 2500,
+        useNewUrlParser: true
+    }).catch(err => {
+        console.error(err.stack);
+        process.exit(1);
+    })
+
