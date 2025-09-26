8db6062478b784cb27fad0311985cefc812e04db
Bethany-23 <bethanyaddis7@gmail.com>
2025-09-26 23:16:57 +0300

    login functionality

--- dev_connect/frontend/js/login.js
+++ dev_connect/frontend/js/login.js
@@ -1,2 +1,9 @@
 
+let username = document.getElementById("_one");
+let password = document.getElementById("_two");
+let login = document.getElementById("login");
 
+
+login.addEventListener(onclick, {
+    
+})
