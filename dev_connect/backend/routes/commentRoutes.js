e34d7dc3911ffabaf1fe10b9687b16d2c93da7bf
Bethany-23 <bethanyaddis7@gmail.com>
2025-10-16 23:32:22 +0300

    routes added

---
+++ dev_connect/backend/routes/commentRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { authMiddleware } = require("../middleware/authMiddleware");
+const { createComment, getComments, deleteComment } = require("../controllers/commentController");
+
+router.post("/", authMiddleware, createComment);
+router.get("/:postId", getComments);
+router.delete("/:id", authMiddleware, deleteComment);
+
+module.exports = router;
---
+++ dev_connect/backend/routes/postRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { authMiddleware, adminOnly } = require("../middleware/authMiddleware");
+const { createPost, getPosts, updatePost, deletePost } = require("../controllers/postController");
+
+router.post("/", authMiddleware, createPost);
+router.get("/", getPosts);
+router.put("/:id", authMiddleware, adminOnly, updatePost);
+router.delete("/:id", authMiddleware, adminOnly, deletePost);
+
+module.exports = router;
