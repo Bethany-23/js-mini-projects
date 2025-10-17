3907e53ec3b1b456a46b50e1e0eec2dc91bfb55d
Bethany-23 <bethanyaddis7@gmail.com>
2025-10-17 22:22:11 +0300

    updates made

--- dev_connect/backend/routes/postRoutes.js
+++ dev_connect/backend/routes/postRoutes.js
@@ -1,7 +1,8 @@
+
 const express = require("express");
 const router = express.Router();
-const { authMiddleware, adminOnly } = require("../middleware/authMiddleware");
-const { createPost, getPosts, updatePost, deletePost } = require("../controllers/postController");
+const {authMiddleware, adminOnly} = require("../middleware/authMiddleware");
+const {createPost, updatePost, getPosts, deletePost} = require("../controllers/postController")
 
 router.post("/", authMiddleware, createPost);
 router.get("/", getPosts);
