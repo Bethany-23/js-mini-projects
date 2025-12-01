b88be7d8ca2ee7aad9b06aed106f2af632340fb7
Bethany <bethanyaddis7@gmail.com>
2025-12-01 21:12:44 +0300

    notes model added

--- Bookish_remade/bookish_backend/src/models/Notes.js
+++ Bookish_remade/bookish_backend/src/models/Notes.js
@@ -1,1 +1,12 @@
 
+import mongoose from "mongoose";
+
+const noteShema = new mongoose.Schema({
+    title: {type: String, required: true},
+    id: {type: Number},
+    createdBy: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    createdFor: {type: mongoose.Schema.Types.ObjectId, ref: "Book", required: true}
+
+})
+
+module.exports = mongoose.model("Note", noteShema);
