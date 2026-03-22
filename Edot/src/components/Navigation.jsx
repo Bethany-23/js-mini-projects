e42d78a9a7cb43c0fa9eeb65ff1a06aef3e38cc5
Bethany Addis <bethanyaddis7@gmail.com>
2026-03-23 00:16:25 +0300

    navigation added

---
+++ Edot/src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function Nav() {
+  // 1. Monitor the length of the favorites array
+  const favCount = useSelector((state) => state.favorites.length);
+
+  return (
+    <nav
+      style={{
+        display: "flex",
+        gap: "20px",
+        padding: "10px",
+        background: "#eee",
+      }}
+    >
+      <Link to="/">Home</Link>
+      <Link to="/fav">
+        Favorites {favCount > 0 && <span>({favCount})</span>}
+      </Link>
+    </nav>
+  );
+}
