af05911760da42b7f58404f3c465911cf37438bb
Bethany Addis <bethanyaddis7@gmail.com>
2026-03-30 22:51:27 +0300

    edited file

--- list_tracker/src/App.jsx
+++ list_tracker/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 function App(){
-  const [task, setTask] = useState([]);
-  const [inputValue, setInputValue] = useState("")
+  const[task, setTask] = useState([]);
+  const[inputValue, setInputValue] = useState("")
 
-  const addTask = () =>{
-    if(inputValue.trim() !== " "){
+  const addTask = ()=>{
+    if(inputValue.trim() !== ""){
       setTask([...task, inputValue]);
       setInputValue("");
     }
   }
-  const removeTask = (indexToRemove)=>{
-    
-  }
-  const updateTask = (index) =>{
+  const removeTask = ()=>{
 
   }
   return(
     <>
+    <h1> Here are your tasks</h1>
     <input
-     type="text"
-     value={inputValue}
-     onChange={(e)=>setInputValue(e.target.value)}/>
+    type="text"
+    placeholder="add your task here"
+    value={inputValue}
+    onChange={(e) =>setInputValue(e.target.value)}/>
 
-    <button onClick={addTask}>Add a task</button>
-    <ul>
-      {task.map((t, index)=>(<li></li>))}
-    </ul>
+    <button onClick={addTask}>Add Task</button>
+    <ul></ul>
     </>
   )
 }
+
 export default App;
