c0690aa0b446c66fae2362a390aecb9c8c619979
Bethany Addis <bethanyaddis7@gmail.com>
2026-03-31 23:27:10 +0300

    edited page

--- list_tracker/src/App.jsx
+++ list_tracker/src/App.jsx
@@ -6,26 +6,40 @@ function App(){
 
   const addTask = ()=>{
     if(inputValue.trim() !== ""){
-      setTask([...task, inputValue]);
+      const newTask= [...task, inputValue];
+      setTask(newTask)
       setInputValue("");
+      localStorage.setItem("myTodoList", JSON.stringify(newTask))
+    };
+  }
+  const removeTask = (index)=>{
+    if(index == task){
+      setTask[index].pop();
     }
   }
-  const removeTask = ()=>{
-
-  }
-  return(
+ 
+  return (
     <>
-    <h1> Here are your tasks</h1>
-    <input
-    type="text"
-    placeholder="add your task here"
-    value={inputValue}
-    onChange={(e) =>setInputValue(e.target.value)}/>
+      <h1> Here are your tasks</h1>
+      <input
+        type="text"
+        placeholder="add your task here"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
 
-    <button onClick={addTask}>Add Task</button>
-    <ul></ul>
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {task.map((t, index) => (
+          <li>
+            {t}
+            <button onClick={()=>removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
 export default App;
