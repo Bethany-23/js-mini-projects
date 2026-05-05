63dec44323e677074c2a6bc36b4adf2fcaf12499
Bethany Addis <bethanyaddis7@gmail.com>
2026-05-05 15:48:52 +0300

    changes made

--- list_tracker/src/App.jsx
+++ list_tracker/src/App.jsx
@@ -1,50 +1,4 @@
-// import { useState, useEffect } from "react";
-
 import { useState } from "react";
-
-// function App(){
-//   const[task, setTask] = useState([]);
-//   const[inputValue, setInputValue] = useState("")
-
-//   const addTask = ()=>{
-//     if(inputValue.trim() !== ""){
-//       const newTask= [...task, inputValue];
-//       setTask(newTask)
-//       setInputValue("");
-//       localStorage.setItem("myTodoList", JSON.stringify(newTask))
-//     };
-//   }
-//   const removeTask = (indexToRemove)=>{
-//     const updatedTask = task.filter((_, index)=> index !== indexToRemove);
-//     setTask(updatedTask);
-//   }
- 
-//   return (
-//     <>
-//       <h1> Here are your tasks</h1>
-//       <input
-//         type="text"
-//         placeholder="add your task here"
-//         value={inputValue}
-//         onChange={(e) => setInputValue(e.target.value)}
-//       />
-
-//       <button onClick={addTask}>Add Task</button>
-
-//       <ul>
-//         {task.map((t, index) => (
-//           <li>
-//             {t}
-//             <button onClick={()=>removeTask(index)}>Remove</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// }
-
-// export default App;
-
 
 
 function App(){
@@ -53,18 +7,18 @@ function App(){
   const[input, setInput] = useState("")
 
   const addTask = () =>{
-
-  }
-
-  const removeTask = () =>{
-    if (input.trim() !== ""){
-      const newTask = [...tasks, input];
-      setTasks(newTask);
-      setInput("");
-      localStorage.getItem()
+      if(input.trim() !== ""){
+        const newTask = [...tasks, input];
+        setTasks(newTask)
+        setInput("")
+        localStorage.setItem("myTodoList", JSON.stringify(newTask))
+      };
     }
 
-  }
+  const removeTask = (indexToRemove) =>{
+    const updatedTask = tasks.filter((_, index) => index !== indexToRemove);
+    setTasks(updatedTask);
+    }
 
   return(
     <>
@@ -77,8 +31,18 @@ function App(){
     onChange={(e)=>setInput(e.target.value)}
      />
     <button onClick={addTask}> Add Task</button>
+
+    <ul>
+      {tasks.map((t, index)=>(
+        <li>
+          {t}
+          <button onClick={()=>removeTask(index)}> Remove</button>
+        </li>
+      ))}
+    </ul>
+
     </>
-  )
+  );
 
 }
 
